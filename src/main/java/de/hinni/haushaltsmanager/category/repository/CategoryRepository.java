95485ac736bc7f9dc66966b6ec65af005d064f70
Hinnerk Siemsen <hinnisiemsen@gmail.com>
2025-11-18 15:31:56 +0100

    backend: Fixes and annotations

--- src/main/java/de/hinni/haushaltsmanager/category/repository/CategoryRepository.java
+++ src/main/java/de/hinni/haushaltsmanager/category/repository/CategoryRepository.java
@@ -2,6 +2,9 @@ package de.hinni.haushaltsmanager.category.repository;
 
 import de.hinni.haushaltsmanager.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
