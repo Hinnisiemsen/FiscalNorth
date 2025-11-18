e40745e49ec8784c1704e490ab6bba99f1539e5a
Hinnerk Siemsen <hinnisiemsen@gmail.com>
2025-11-18 15:31:41 +0100

    backend: Started Implementatation of PaymentTransactionService with corresponding PaymentTransactionRepository methods

--- src/main/java/de/hinni/haushaltsmanager/transaction/repository/PaymentTransactionRepository.java
+++ src/main/java/de/hinni/haushaltsmanager/transaction/repository/PaymentTransactionRepository.java
@@ -1,7 +1,23 @@
 package de.hinni.haushaltsmanager.transaction.repository;
 
+import de.hinni.haushaltsmanager.category.model.Category;
 import de.hinni.haushaltsmanager.transaction.model.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, Long> {
+    List<PaymentTransaction> findPaymentTransactionByCategory(Category category);
+
+    List<PaymentTransaction> findPaymentTransactionByAmountGreaterThan(BigDecimal amountIsGreaterThan);
+
+    @Query("select pt from PaymentTransaction pt where pt.tags like %:tag%")
+    List<PaymentTransaction> findPaymentTransactionByTagsIs(@Param("tag") String tag);
+
+
 }
