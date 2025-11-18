b5dc51e6d1cdd8414c1a1a4c90f5e1903ec24301
Hinnerk Siemsen <hinnisiemsen@gmail.com>
2025-11-18 15:29:18 +0100

    backend: Created GlobalExceptionHandler with ApiError generic type and new Exceptions

---
+++ src/main/java/de/hinni/haushaltsmanager/shared/RessourceNotFoundException.java
@@ -0,0 +1,11 @@
+package de.hinni.haushaltsmanager.shared;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RessourceNotFoundException extends RuntimeException {
+    public RessourceNotFoundException(String ressourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s nicht gefunden mit %s: '%s'", ressourceName, fieldName, fieldValue));
+    }
+}
