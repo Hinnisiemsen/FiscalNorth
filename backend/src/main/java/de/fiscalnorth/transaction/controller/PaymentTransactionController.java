36f134841a9e9a81a464b1c243e02d2394378dd0
Hinnerk Siemsen <hinnisiemsen@gmail.com>
2025-11-27 22:14:44 +0100

    feat: update PaymentTransactionController with new endpoints

--- backend/src/main/java/de/fiscalnorth/transaction/controller/PaymentTransactionController.java
+++ backend/src/main/java/de/fiscalnorth/transaction/controller/PaymentTransactionController.java
@@ -1,26 +1,45 @@
 package de.fiscalnorth.transaction.controller;
 
+import de.fiscalnorth.category.model.Category;
+import de.fiscalnorth.category.service.CategoryService;
 import de.fiscalnorth.transaction.dto.CreatePaymentTransactionRequest;
 import de.fiscalnorth.transaction.model.PaymentTransaction;
 import de.fiscalnorth.transaction.service.PaymentTransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transaction/payment")
 public class PaymentTransactionController {
     private final PaymentTransactionService paymentTransactionService;
+    private final CategoryService categoryService;
 
-    public PaymentTransactionController(PaymentTransactionService paymentTransactionService) {
+    public PaymentTransactionController(PaymentTransactionService paymentTransactionService,
+            CategoryService categoryService) {
         this.paymentTransactionService = paymentTransactionService;
+        this.categoryService = categoryService;
     }
 
     @PostMapping
-    public ResponseEntity<PaymentTransaction> createPaymentTransaction(CreatePaymentTransactionRequest paymentTransactionRequest) {
-        PaymentTransaction paymentTransaction = paymentTransactionService.createPaymentTransaction(paymentTransactionRequest);
+    public ResponseEntity<PaymentTransaction> createPaymentTransaction(
+            CreatePaymentTransactionRequest paymentTransactionRequest) {
+        PaymentTransaction paymentTransaction = paymentTransactionService
+                .createPaymentTransaction(paymentTransactionRequest);
         return new ResponseEntity<>(paymentTransaction, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<PaymentTransaction>> getPaymentTransactionsByCategory(@PathVariable Long categoryId) {
+        Category category = categoryService.getCategory(categoryId);
+        return ResponseEntity.ok(paymentTransactionService.getExpensesBySingleCategory(category));
+    }
+
+    @GetMapping("/amount/{amount}")
+    public ResponseEntity<List<PaymentTransaction>> getPaymentTransactionsOverAmount(@PathVariable BigDecimal amount) {
+        return ResponseEntity.ok(paymentTransactionService.getExpensesOverValue(amount));
     }
 }
