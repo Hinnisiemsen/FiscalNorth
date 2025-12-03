340da57243d4024b5767b2370b0928ec61acdb22
Hinnerk Siemsen <hinnisiemsen@gmail.com>
2025-12-03 20:10:43 +0100

    fix error where user table is already used

--- backend/src/main/java/de/fiscalnorth/user/model/User.java
+++ backend/src/main/java/de/fiscalnorth/user/model/User.java
@@ -2,12 +2,14 @@ package de.fiscalnorth.user.model;
 
 import de.fiscalnorth.shared.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "app_user")
 public class User extends BaseEntity {
     protected String userName;
     protected String email;
